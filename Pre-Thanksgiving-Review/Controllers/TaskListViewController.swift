cf8634d29ff9737f9b8425d04dede3a64fecb25f
Olimpia <olimpiacompean@prusuit.org>
2019-11-21 10:06:30 -0500

    first commit

---
+++ Pre-Thanksgiving-Review/Controllers/TaskListViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class TaskListViewController: UIViewController {
+ 
+    let tableView = MainView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegatesAndAddedViews()
+       
+    }
+    
+    func delegatesAndAddedViews() {
+        self.view.addSubview(tableView)
+        tableView.tableView.delegate = self
+        tableView.tableView.dataSource = self
+    }
+
+
+}
+
+
+extension TaskListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as? TasksCell else { return UITableViewCell() }
+        cell.taskLabel.text = "test"
+        return cell
+    }
+    
+    
+}
